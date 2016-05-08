f5c93ced1a2faa7d9ccf2dd62869e9aa23c9ed70
usuario <usuario@toshiba>
2016-05-08 13:00:49 -0700

    :D

--- src/Personajes/Aliado.java
+++ src/Personajes/Aliado.java
@@ -11,4 +11,47 @@ package Personajes;
  */
 public class Aliado {
     
+    String nombre;
+    int vida;
+    int daño;
+    
+   public Aliado()
+   {
+    this.nombre="Aliado desconocido";
+    this.vida =100;
+    this.daño= 10;
+   }
+   
+   public Aliado(String Nombre,int Vida, int Daño)
+   {
+    this.nombre=Nombre;
+    this.vida =Vida;
+    this.daño= Daño;
+   }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+    
+    
 }
