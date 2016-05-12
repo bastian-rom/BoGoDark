efc9768d4b1db954e67ee1bd39ab9db05d6d0fdd
williamCBO <williamcamilobeltranoyola@gmail.com>
2016-05-11 22:04:53 -0500

    Update NivelNe.java

--- src/Niveles/NivelNe.java
+++ src/Niveles/NivelNe.java
@@ -11,4 +11,73 @@ package Niveles;
  */
 public class NivelNe {
     
+   String nivel_10;
+   int incremento_vida;
+   int incremento_daño;
+   int incremento_defensa;
+   boolean habilidad_especial;
+   
+
+ public NivelNe()
+{
+this.nivel_10="nivel 10";
+this.incremento_vida= 20;
+this.incremento_daño= 20;
+this.incremento_defensa=20;
+this.habilidad_especial=true;
+
+/* la habilidad especial= aumenta la visibilidad del usuario
+ya que el neon utilizado como bombilla en bares*/
+
+}
+ public NivelNe (String Nivel_10,int IncrementoVida,int IncrementoDaño,int IncrementoDefensa,boolean HabilidadEspecial) 
+ {
+     this.nivel_10=Nivel_10;
+     this.incremento_vida=IncrementoVida;
+     this.incremento_daño=IncrementoDaño;
+     this.incremento_defensa=IncrementoDefensa;
+     this.habilidad_especial=HabilidadEspecial;
+     
+ } 
+
+    public String getNivel_10() {
+        return nivel_10;
+    }
+
+    public void setNivel_10(String nivel_10) {
+        this.nivel_10 = nivel_10;
+    }
+
+    public int getIncremento_vida() {
+        return incremento_vida;
+    }
+
+    public void setIncremento_vida(int incremento_vida) {
+        this.incremento_vida = incremento_vida;
+    }
+
+    public int getIncremento_daño() {
+        return incremento_daño;
+    }
+
+    public void setIncremento_daño(int incremento_daño) {
+        this.incremento_daño = incremento_daño;
+    }
+
+    public int getIncremento_defensa() {
+        return incremento_defensa;
+    }
+
+    public void setIncremento_defensa(int incremento_defensa) {
+        this.incremento_defensa = incremento_defensa;
+    }
+
+    public boolean isHabilidad_especial() {
+        return habilidad_especial;
+    }
+
+    public void setHabilidad_especial(boolean habilidad_especial) {
+        this.habilidad_especial = habilidad_especial;
+    }
+ 
 }
