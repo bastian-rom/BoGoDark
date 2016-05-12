7ea43e64c4bacdccb73ead8276450fd0eb83e33d
jeison velasco <daviddaza485@gmail.com>
2016-05-12 18:20:43 -0500

    Update ArmasQuimicas.java

--- src/Armas/ArmasQuimicas.java
+++ src/Armas/ArmasQuimicas.java
@@ -10,5 +10,28 @@ package Armas;
  * @author Sebastian
  */
 public class ArmasQuimicas {
+     int alcance;
+    int velocidadcarga;
+    int daño;
+    int velocidadataque;
+    String nombre;
+    int municion;
     
+    
+     public ArmasQuimicas()
+ {
+     this.alcance=3;
+     this.daño=5;
+     this.velocidadataque=3;
+     this. nombre="arma quimica";
+     this.velocidadcarga=4;
+ }
+ public class ArmasBlancas(int Alcance, int Daño, int Velocidadataque, String Nombre, int Velocidadcarga)
+ {
+     this.alcance=Alcance;
+     this.daño=Daño;
+     this.velocidadataque=Velocidadataque;
+     this.velocidadcarga=Velocidadcarga;
+     
+ }
 }
