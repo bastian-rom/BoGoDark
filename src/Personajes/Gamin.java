5d4f291198dcb50f0c6c6da12bd233b93b91e460
williamCBO <williamcamilobeltranoyola@gmail.com>
2016-05-11 22:12:15 -0500

    Update Gamin.java

--- src/Personajes/Gamin.java
+++ src/Personajes/Gamin.java
@@ -7,8 +7,75 @@ package Personajes;
 
 /**
  *
- * @author Sebastian
+ * @author ttaa
  */
 public class Gamin {
-    
+     String nombre;
+    int vida;
+    int daño;
+    int defensa;
+    boolean hostil;
+
+public Gamin() 
+{
+
+this.nombre="desconocido";
+this.vida=100;
+this.daño=15;
+this.defensa=10;
+this.hostil=true;
+}
+public Gamin(String Nombre,int Vida,int Daño,int Defensa,boolean Hostil)
+        
+{
+       this.nombre=Nombre;
+       this.daño=Daño;
+       this.defensa=Defensa;
+       this.vida=Vida;
+       this.hostil=Hostil;
+}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public boolean isHostil() {
+        return hostil;
+    }
+
+    public void setHostil(boolean hostil) {
+        this.hostil = hostil;
+    }
+
+    public int setVida() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
