4f406f28da0f72bc73cb4c0559488b91a9b2d995
xX-bald-Xx <jeissonaldanamora@gmail.com>
2016-05-12 15:38:46 -0500

    Update Ejercito.java

--- src/Personajes/Ejercito.java
+++ src/Personajes/Ejercito.java
@@ -10,6 +10,69 @@ package Personajes;
  * @author Sebastian
  */
 public class Ejercito {
+    String nombre;
+    int vida;
+    int daño;
+    int defensa;
+    int autoridad;
     
+    public Ejercito()
+    {
+        this.nombre="coronel";
+        this.vida=100;
+        this.daño=35;
+        this.defensa=20;
+        this.autoridad=100;
+    }
+    public Ejercito(int Vida,String Nombre,int Daño,int Defensa,int Autoridad)
+    {
+        this.nombre=Nombre;
+        this.vida=Vida;
+        this.daño=Daño;
+        this.defensa=Defensa;
+        this.autoridad=Autoridad;
+        
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getAutoridad() {
+        return autoridad;
+    }
+
+    public void setAutoridad(int autoridad) {
+        this.autoridad = autoridad;
+    }
 }
-/*[clase editada por bald]*/
+/*[editada por jeisson aldana mora]*/
