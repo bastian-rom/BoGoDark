054dd96bf614ad54d985d508d446d5959bbe30f1
xX-bald-Xx <jeissonaldanamora@gmail.com>
2016-05-12 15:27:58 -0500

    Update Guerrillero.java

--- src/Personajes/Guerrillero.java
+++ src/Personajes/Guerrillero.java
@@ -3,13 +3,89 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Personajes;
+
 
 /**
  *
  * @author Sebastian
  */
+
+    package Personajes;
+
+/**
+ *
+ * @author Sebastian
+ */
+ /*editada por jeisson aldana mora*/
 public class Guerrillero {
+    String nombre;
+    int vida;
+    int daño;
+    int defensa;
+    int autoridad;
     
-}
-/*[eidtada por bald]*/
+  public Guerrillero()
+          
+  {
+  this.nombre="cano";
+  this.vida=100;
+  this.daño=15;
+  this.defensa=25;
+  this.autoridad=0;
+          
+  }
+  public Guerrillero(int Vida,String Nombre,int Daño,int Defensa,int Autoridad)
+  { 
+      this.nombre=Nombre;
+      this.vida=Vida;
+      this.daño=Daño;
+      this.defensa=Defensa; 
+      this.autoridad=Autoridad;
+              
+  }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getAutoridad() {
+        return autoridad;
+    }
+
+    public void setAutoridad(int autoridad) {
+        this.autoridad = autoridad;
+    }
+}       
+
+             
+
+    
+
