2dbabec459bd77a5509e09a62144c1b93c5c11a1
xX-bald-Xx <jeissonaldanamora@gmail.com>
2016-05-12 16:45:17 -0500

    Update ArmaduraManos.java

--- src/Armadura/ArmaduraManos.java
+++ src/Armadura/ArmaduraManos.java
@@ -10,6 +10,68 @@ package Armadura;
  * @author Sebastian
  */
 public class ArmaduraManos {
+    String material;
+    int defensa;
+    int dureza;
+    float peso;
+    int daño;
     
+    public ArmaduraManos()
+            
+    {        
+        this.material="cobre";
+        this.defensa=30;
+        this.dureza=50;
+        this.peso=10;
+        this.daño=35; 
+    }
+    public ArmaduraManos(int Defensa,String Material,int Dureza,float Peso,int Daño)
+    {
+        this.material=Material;
+        this.defensa=Defensa;
+        this.dureza=Dureza;
+        this.peso=Peso;
+        this.daño=Daño;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getDureza() {
+        return dureza;
+    }
+
+    public void setDureza(int dureza) {
+        this.dureza = dureza;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
 }
-/*editada por bald*/
+/*editada jeisson aldana mora*/
