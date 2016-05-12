d5049c5979efc38bad4bfd6ea9a3cf99de0119d7
xX-bald-Xx <jeissonaldanamora@gmail.com>
2016-05-12 15:56:50 -0500

    Update Policia.java

--- src/Personajes/Policia.java
+++ src/Personajes/Policia.java
@@ -10,6 +10,78 @@ package Personajes;
  * @author Sebastian
  */
 public class Policia {
+    String nombre;
+    int vida;
+    int daño;
+    int defensa;
+    int autoridad;
+    boolean amigo;
     
+   public Policia()
+   {
+       this.nombre="general";
+       this.vida=100;
+       this.daño=35;
+       this.defensa=20;
+       this.autoridad=100;
+       this.amigo=true;      
+   }       
+    public Policia(int Vida, String Nombre,int Daño,int Defensa,int Autoridad)
+    {
+        this.nombre=Nombre;
+        this.vida=Vida;
+        this.daño=Daño;
+        this.defensa=Defensa;
+        this.autoridad=Autoridad;
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getAutoridad() {
+        return autoridad;
+    }
+
+    public void setAutoridad(int autoridad) {
+        this.autoridad = autoridad;
+    }
+
+    public boolean isAmigo() {
+        return amigo;
+    }
+
+    public void setAmigo(boolean amigo) {
+        this.amigo = amigo;
+    }
 }
-/*[editada por bald]*/
+/*[editada jeisson aldana mora]*/
