58008e4a60ce397476342764fe2b58f5b1a6d45c
williamCBO <williamcamilobeltranoyola@gmail.com>
2016-05-11 22:11:09 -0500

    Update Bombero.java

--- src/Personajes/Bombero.java
+++ src/Personajes/Bombero.java
@@ -10,5 +10,69 @@ package Personajes;
  * @author Sebastian
  */
 public class Bombero {
-    
+   
+    String nombre;
+    int vida;
+    int daño;
+    int defensa;
+    boolean hostil;
+
+public Bombero() 
+{
+
+this.nombre="desconocido";
+this.vida=100;
+this.daño=15;
+this.defensa=10;
+this.hostil=false;
+}
+public Bombero(String Nombre,int Vida,int Daño,int Defensa,boolean Hostil)
+        
+{
+       this.nombre=Nombre;
+       this.daño=Daño;
+       this.defensa=Defensa;
+       this.vida=Vida;
+       this.hostil=Hostil;
+}
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public int getDefenza() {
+        return defensa;
+    }
+
+    public void setDefenza(int defenza) {
+        this.defensa = defenza;
+    }
+
+    public boolean isHostil() {
+        return hostil;
+    }
+
+    public void setHostil(boolean hostil) {
+        this.hostil = hostil;
+    }
+
 }
