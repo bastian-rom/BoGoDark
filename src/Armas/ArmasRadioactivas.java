96d1a3aa2cf65e431f261fb6daf2588e7cc60052
jeison velasco <daviddaza485@gmail.com>
2016-05-12 20:06:49 -0500

    Update ArmasRadioactivas.java

--- src/Armas/ArmasRadioactivas.java
+++ src/Armas/ArmasRadioactivas.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+editado por jeiso velasco.
  */
 package Armas;
 
@@ -10,5 +8,43 @@ package Armas;
  * @author Sebastian
  */
 public class ArmasRadioactivas {
+    int alcance;
+    int velocidad;
+    int daño;
+    int municion;
+    int cantidadataque;
+    String nombre;
+    boolean equipado;
+    
+    public ArmasRadioactivas ()
+    {
+        this.alcance=10;
+        this.velocidad=8;
+        this.daño=20;
+        this.municion=15;
+        this.cantidadataque=20;
+        this.equipado=false;
+        
+        
+    }
+    
+    public ArmasRadioactivas(int Alcance, int Velocidad, int Daño, int Municion, int Cantidadataque, String Nombre, boolean Equipado )
+    {
+        this.alcance=Alcance;
+        this.velocidad=Velocidad;
+        this.daño=Daño;
+        this.municion=Municion;
+        this.cantidadataque=Cantidadataque;
+        this.equipado=Equipado;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
