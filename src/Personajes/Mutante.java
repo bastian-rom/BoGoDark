f5bb39abb0ddcea6c72606f354c19e05a221be97
jeison velasco <daviddaza485@gmail.com>
2016-05-12 20:18:12 -0500

    Update Mutante.java

--- src/Personajes/Mutante.java
+++ src/Personajes/Mutante.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +11,29 @@ package Personajes;
  * @author Sebastian
  */
 public class Mutante {
-    
+String nombre;
+    int vida;
+    int daño;
+    int peligro;
+
+
+  public Mutante()
+          
+  {
+  this.nombre="super mutante";
+  this.vida=100;
+  this.daño=15;
+  this.peligro=25;
+ 
+          
+  }
+  public Guerrillero( int Vida, String Nombre, int Daño, int Peligro)
+  { 
+      this.nombre=Nombre;
+      this.vida=Vida;
+      this.daño=Daño;
+      this.peligro=Peligro
+              
+  }
+
 }
