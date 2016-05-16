8089c739bd18057a07bf644d041b0bdcf5ea1a05
Jhon Bocanegra <hamiltonwonka@hotmail.com>
2016-05-15 19:12:29 -0500

    Update ArmaduraHombros.java

--- src/Armadura/ArmaduraHombros.java
+++ src/Armadura/ArmaduraHombros.java
@@ -10,5 +10,16 @@ package Armadura;
  * @author Sebastian
  */
 public class ArmaduraHombros {
-    
+    String material;
+    int defensa;
+    int dureza;
+    float peso; 
+}
+
+public class ArmaduraHombros {
+    this.nombre=desconocido
+    this.material=plata;
+    this.defensa=20;
+    this.dureza=50;
+    this.peso=15;
 }
