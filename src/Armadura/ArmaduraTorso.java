76e164fd5d2bb13a8108cb52b623a85fdbc5ecd2
Jhon Bocanegra <hamiltonwonka@hotmail.com>
2016-05-15 19:27:12 -0500

    Update ArmaduraTorso.java

--- src/Armadura/ArmaduraTorso.java
+++ src/Armadura/ArmaduraTorso.java
@@ -10,5 +10,63 @@ package Armadura;
  * @author Sebastian
  */
 public class ArmaduraTorso {
-    
+    String material;
+    int defensa;
+    int dureza;
+    float peso; 
+
+public class ArmaduraTorso {
+    this.nombre=desconocido
+    this.material="Platino";
+    this.defensa=100;
+    this.dureza=50;
+    this.peso=25;
 }
+ public ArmaduraTorso(int Defensa,String Material,int Dureza,float Peso,int Daño){
+    this.material=Material;
+    this.defensa=Defensa;
+    this.dureza=Dureza;
+    this.peso=Peso;
+    this.daño=Daño;
+    }
+    
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getDureza() {
+        return dureza;
+    }
+
+    public void setDureza(int dureza) {
+        this.dureza = dureza;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
