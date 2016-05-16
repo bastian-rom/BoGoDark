bbf1bec1f74f43f0799b3ff6f7b3825fe699e9eb
jeison velasco <daviddaza485@gmail.com>
2016-05-16 08:47:24 -0500

    Update Mutante.java

--- src/Personajes/Mutante.java
+++ src/Personajes/Mutante.java
@@ -27,13 +27,46 @@ String nombre;
  
           
   }
-  public Guerrillero( int Vida, String Nombre, int Daño, int Peligro)
+  public Mutante( int Vida, String Nombre, int Daño, int Peligro)
   { 
       this.nombre=Nombre;
       this.vida=Vida;
       this.daño=Daño;
-      this.peligro=Peligro
+      this.peligro=Peligro;
               
   }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public int getPeligro() {
+        return peligro;
+    }
+
+    public void setPeligro(int peligro) {
+        this.peligro = peligro;
+    }
+
 }
+
