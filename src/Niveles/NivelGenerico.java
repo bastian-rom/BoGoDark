b3e33ef9d53aa45cbdd4f9982e6ae93081229676
Usuario <Usuario@Usuario-PC>
2016-06-13 10:10:03 -0500

    holas

---
+++ src/Niveles/NivelGenerico.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Niveles;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NivelGenerico {
+    private String nivel;
+    private int incremento_vida;
+    private int incremento_damage;
+    private boolean habilidad_especial;
+    
+
+    
+    public NivelGenerico(String Nivel)
+    {
+     this.
+     
+     
+     
+     
+        
+        
+        
+     
+    }
+           
+    
+}
