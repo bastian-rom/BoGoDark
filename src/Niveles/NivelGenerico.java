278caa1913f5dcbb99e2e9100e33edd3822b7c7b
jeison velasco <>
2016-06-15 17:09:15 -0500

    hola que hace

--- src/Niveles/NivelGenerico.java
+++ src/Niveles/NivelGenerico.java
@@ -1,17 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 package Niveles;
 
 import bogodark.Dados;
 
 
-/**
- *
- * @author Usuario
- */
+
 public class NivelGenerico {
     private String nivel;
     private int incremento_vida;
